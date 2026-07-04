bf7c3a6e97da727a1942717bac27c3807bd2fe51
Arthurrosa1 <arthurrosa3849@gmail.com>
2026-07-04 14:42:38 -0300

    changes

--- projeto-marmoraria/src/routes/AppRoutes.jsx
+++ projeto-marmoraria/src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "../pages/Login/Login";
-import Dashboard from "../pages/dashboard/dashboard";
+import Dashboard from "../pages/Dashboard/Dashboard";
 
 function AppRoutes() {
   return (
